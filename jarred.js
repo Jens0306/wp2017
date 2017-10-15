c1fdfd172f60cd7db3be9d0cb4c4f4b0d7184df3
jens0306 <ace020771@gmail.com>
2017-10-15 20:03:48 +0800

    test

--- jarred.js
+++ jarred.js
@@ -1,11 +1,4 @@
-function moveUp(_a){
-  var _row = _a.parentNode.parentNode;
-  //如果不是第一行，則與上一行交換順序
-  var _node = _row.previousSibling;
-  while(_node && _node.nodeType != 1){
-    _node = _node.previousSibling;
-  }
-  if(_node){
-    swapNode(_row,_node);
-  }
-}
+$("document").onload(function(){
+    $("td#about_me").slideToggle("slow");
+  });
+});
