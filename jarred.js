61d8c5bcc068d94c53f317057c283d4c26522c97
jens0306 <ace020771@gmail.com>
2017-10-16 18:42:37 +0800

    test

--- jarred.js
+++ jarred.js
@@ -1,5 +1,7 @@
-$("document").click(function(){
+$("document").ready(function(){
+  $("#menu").click(function(){
     $("#about_me").toggle("slide");
+  });
 });
 function myFunction() {
     alert("Hello\nHow are you?");
