f66297029e2d716ae1de3d88f4420cbebb38db0e
jens0306 <ace020771@gmail.com>
2017-10-16 18:57:25 +0800

    test

--- jarred.js
+++ jarred.js
@@ -1,8 +1,6 @@
 $("document").ready(function(){
   $("#menu").click(function(){
-    $("#about_me").toggle("slide");
-    $("#contact").toggle("slide");
-    $("#skill").toggle("slide");
+    $("#about_me #contact #skill").toggle("slide");
   });
 });
 function myFunction() {
