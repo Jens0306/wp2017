dde97467281b6439c82080bc7a4ac1e78c0abc3f
Jens0306 <ace67890@yahoo.com.tw>
2017-10-18 02:21:37 +0800

    js

--- jarred.js
+++ jarred.js
@@ -4,19 +4,19 @@ $("document").ready(function(){
     var flag = 0;
     $("#layer3").animate({"background-position":"-1550px"}, 1000);
     $("#layer1").animate({"background-position":"500px"}, 1000);
-    $("#layer2").animate({"background-position":"-1120px"}, 1000);
+    $("#layer2").animate({"background-position":"-1200px"}, 1000);
     $("p").animate({"opacity":"1"}, 1800);
     $("a").animate({"opacity":"1"}, 1800);
   setInterval(function(){
     if (flag == 0) {
       setTimeout(function(){
-        $("#layer1_0").animate({"background-position":"500px"}, 1000);
-        $("#layer2_0").animate({"background-position":"-1000px"}, 1000);
+        $("#layer1_0").animate({"background-position":"500px"}, 1200);
+        $("#layer2_0").animate({"background-position":"-1150px"}, 1000);
         setTimeout(function(){
           $("#layer1").animate({"background-position":"-1920px"}, 1000);
           $("#layer2").animate({"background-position":"-1920px"}, 1000);
         }, 1000);
-    }, 2000);
+    }, 1000);
     flag++;
     flag %= 2;
   }
