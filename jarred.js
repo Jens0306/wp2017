d11eb59636392b3420a3cf0cfbfbe4cbe1578687
Jens0306 <ace67890@yahoo.com.tw>
2017-10-18 02:11:10 +0800

    js

--- jarred.js
+++ jarred.js
@@ -2,20 +2,21 @@
 $("document").ready(function(){
 //$("#bton").hover(
     var flag = 0;
-    $("#layer3").animate({"background-position":"-1250px"}, 1000);
+    $("#layer3").animate({"background-position":"-1550px"}, 1000);
     $("#layer1").animate({"background-position":"500px"}, 1000);
     $("#layer2").animate({"background-position":"-1120px"}, 1000);
     $("p").animate({"opacity":"1"}, 1800);
     $("a").animate({"opacity":"1"}, 1800);
-  if (flag == 0) {
-    setTimeout(function(){
-      $("#layer1").animate({"background-position":"-1920px"}, 1000);
-      $("#layer2").animate({"background-position":"-1920px"}, 1000);
-
-      $("#layer1").animate({"background-position":"500px"}, 1000);
-      $("#layer2").animate({"background-position":"-1120px"}, 1000);
-
-    }, 3000);
+  setInterval(function(){
+    if (flag == 0) {
+      setTimeout(function(){
+        $("#layer1_0").animate({"background-position":"500px"}, 1000);
+        $("#layer2_0").animate({"background-position":"-1000px"}, 1000);
+        setTimeout(function(){
+          $("#layer1").animate({"background-position":"-1920px"}, 1000);
+          $("#layer2").animate({"background-position":"-1920px"}, 1000);
+        }, 1000);
+    }, 2000);
     flag++;
     flag %= 2;
   }
@@ -27,5 +28,5 @@ $("document").ready(function(){
     flag++;
     flag %= 2;
   }
-
+}, 3000);
 });
